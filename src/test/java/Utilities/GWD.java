dc95dc4b670fb7630227ab8827cfc77fb4646cbb
ibrahimF01 <matrix012>
2022-10-13 03:33:07 +0300

    jenkins test cucumber3

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -46,7 +47,11 @@ public class GWD {
             switch (browserName) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    threadDriver.set(new ChromeDriver()); // bu thread e chrome istenmişşse ve yoksa bir tane ekleniyor
+
+                    ChromeOptions options=new ChromeOptions();//hafızada maximize modda çalıştırmak için eklendi
+                    options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+
+                    threadDriver.set(new ChromeDriver(options)); // bu thread e chrome istenmişşse ve yoksa bir tane ekleniyor
                     break;
 
                 case "firefox":
