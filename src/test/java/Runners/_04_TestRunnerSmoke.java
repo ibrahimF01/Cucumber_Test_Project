97a7ee1cf50c342d10aa0f48d0e33d89720e37de
ibrahimF01 <matrix012>
2022-10-13 03:37:50 +0300

    jenkins test cucumber4

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -7,8 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",//hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
-        plugin = {"html:target//cucumber-reports.html"}
-       // plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+        //plugin = {"html:target//cucumber-reports.html"}
+       plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
 )
 
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
--- src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
+++ src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@Regression",
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
-        plugin = {"html:target//cucumber-reports.html"}
+        //plugin = {"html:target//cucumber-reports.html"}
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
        // monochrome = true
 )
 public class _06_TestRunnerRegressionWithPlugin extends AbstractTestNGCucumberTests {
